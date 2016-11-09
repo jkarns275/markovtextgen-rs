cc6fb07833a00fb66e354c06caca4e7b26fd9871
Josh Karns <jkarns275@gmail.com>
2016-11-08 21:11:07 -0500

    Removed the 'strip' option

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,39 @@
 extern crate rand;
+extern crate regex;
 
 /// Module contains all methods and sturcts related to creating and using Markov
 /// chains to generate text.
 pub mod markov {
+    use regex::Regex;
+    use rand::random;
 
+    /// Creates a new regex with the given &str.
+    /// This method will prevent the need to `use regex::Regex`
+    pub fn regex<'a>(s: &'a str) -> Option<Regex> {
+        if let Ok(reg) = Regex::new(s) {
+            Some(reg)
+        } else {
+            None
+        }
+    }
+
+    /// Returns a regex that will capture all valid URLs
+    pub fn url_regex() -> Regex {
+        Regex::new("(https?://www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%_\\+.~#?&//=]*)").unwrap()
+    }
+
+    /// Returns a regex that will capture all non-alphanumeric and non-whitespace characters
+    pub fn non_alphanumeric_regex() -> Regex {
+        Regex::new(r"[^a-zA-Z\d\s:]").unwrap()
+    }
+
+    /// Returns a regex that will capture all new line characters
+    pub fn newlines_regex() -> Regex {
+        Regex::new(r"[\r\n]+").unwrap()
+    }
+
+    /// An enum contained in the Markov struct that determines what letter case to use,
+    /// or to just leave letter casing as it is.
     #[derive(PartialEq)]
     pub enum LetterCase {
         /// All letters will be made upper case
@@ -20,10 +50,13 @@ pub mod markov {
         seeds: Vec<(String, String)>,
         /// Holds every chain that has been fed into the struct.
         chains: Vec<(String, String, Vec<String>)>,
-        /// Should input be sanatized of non alpha-numeric characters
-        should_strip: bool,
         /// What should be done to input (see LetterCase enum)
-        case: LetterCase
+        case: LetterCase,
+        /// A vector of regexes that will be applied all incoming data in the order that
+        /// they are added. Any match will be REMOVED from the string being operated on.
+        filters: Vec<Regex>,
+        /// A transformation function to be applied to all incoming data
+        transform: Option<Box<Fn(&str) -> String>>,
     }
 
     /// Creates a new empty Markov object.
@@ -31,8 +64,9 @@ pub mod markov {
         Markov {
             seeds : vec![],
             chains: vec![],
-            should_strip: false,
-            case: LetterCase::Any
+            case: LetterCase::Any,
+            transform: None,
+            filters: vec![]
         }
     }
 
@@ -43,26 +77,16 @@ pub mod markov {
         sentence.split_whitespace().collect()
     }
 
-    /// Strips a sentence of all characters that are not alpha-numeric or whitespace
-    /// # Arguments
-    /// * `sentence` - A string slice containing the sentence to be cleaned
-    fn strip<'a>(sentence: &'a str) -> String {
-        let mut s: String = String::from("");
-        for c in sentence.chars() {
-            match c {
-                'a' ... 'z' |
-                'A' ... 'Z' |
-                '0' ... '9' |
-                ' ' | '\n' | '\r' => s.push(c),
-                _ => {}
-            }
-        }
-        s
-    }
-
     impl Markov {
-        /// Generates a string using the data passed into the markov chain
-        /// Returns
+
+        /// Adds a regex to the list of filters that will be applied to all incoming data
+        pub fn add_filter(&mut self, reg: Regex) {
+            self.filters.push(reg);
+        }
+
+        /// Generates a string using the data passed into the markov chain.
+        ///
+        /// ## Returns
         /// * `None` - Will return none if the markov chain is empty
         /// * `Some(sentence)` - Returns Some if the markov chain is not empty,
         ///     the sentnence is not gaurenteed to have the specified length
@@ -70,8 +94,6 @@ pub mod markov {
         /// # Arguments
         /// * `length` - The maximum length of the sentence
         pub fn generate(&self, length: i32) -> Option<String> {
-            use rand::random;
-
             if self.seeds.len() == 0 {
                 return None;
             }
@@ -99,7 +121,7 @@ pub mod markov {
         /// ```
         /// let mut markov = markov::new();
         /// let data = vec!["Hello, how are you?", "What are you going to wear tonight?", "What time is it?"];
-        /// markov.should_strip = true;
+        /// markov.add_filter(markov::non_alphanumeric_regex());
         /// markov.pass(data);
         /// assert!(markov.seeds.contains(&("Hello".to_string(), "how".to_string())));
         /// ```
@@ -107,46 +129,15 @@ pub mod markov {
             for x in sentences {
                 self.pass_str(x);
             }
-
         }
 
-        /// Adds a sentence to the MarkovChain
-        /// # Arguments
-        /// * `sentence` - A string containing the sentence to be added
-        /// # Examples
-        /// ```
-        /// let mut markov = markov::new();
-        /// markov.pass_string("Hello how are you doing today?".to_string());
-        /// assert!(markov.generate(5) == "Hello how are you doign today?".to_string());
-        /// ```
-        pub fn pass_string(&mut self, sentence: String) {
-            let mut words: Vec<String> = words(&sentence).iter().map(|x| x.to_string()).collect();
-            if self.should_strip {
-                for _ in 0..words.len() {
-                    let s: String = words.remove(0);
-                    words.push(strip(&s));
-                }
-            }
-            if self.case == LetterCase::Lower {
-                for _ in 0..words.len() {
-                    let s: String = words.remove(0);
-                    words.push(s.to_lowercase());
-                }
-            } else if self.case == LetterCase::Upper{
-                for _ in 0..words.len() {
-                    let s: String = words.remove(0);
-                    words.push(s.to_uppercase());
-                }
-            }
-            self.seeds.push((words[0].to_string(), words[1].to_string()));
-            for i in 0..words.len()-2 {
-                self.add(&words[i], &words[i + 1], &words[i + 2]);
-            }
-        }
-
-        /// Adds a sentence to the MarkovChain
+        /// Adds a sentence to the MarkovChain. Any specified transformations
+        /// specified will be performed in following order: transformation function,
+        /// filters (regex), and finally letter capitalization
         /// # Arguments
         /// * `sentence` - A &str containing the sentence to be added
+        /// # Panics
+        /// * Will panic if an invalid regex is contained in markov.filters
         /// # Examples
         /// ```
         /// let mut markov = markov::new();
@@ -155,24 +146,26 @@ pub mod markov {
         /// ```
         pub fn pass_str<'a>(&mut self, sentence: &'a str) -> bool {
             let mut words: Vec<String> = words(sentence).iter().map(|x| x.to_string()).collect();
-            if self.should_strip {
-                for _ in 0..words.len() {
-                    let s: String = words.remove(0);
-                    words.push(strip(&s));
+            for f in 0..self.filters.len() {
+                let ref re = self.filters[f];
+                for i in 0..words.len() {
+                    words[i] = re.replace_all(&words[i], "").to_string();
+                }
+            }
+            if let Some(ref f) = self.transform {
+                for i in 0..words.len() {
+                    words[i] = f(&words[i]);
                 }
             }
             if self.case == LetterCase::Lower {
-                for _ in 0..words.len() {
-                    let s: String = words.remove(0);
-                    words.push(s.to_lowercase());
+                for i in 0..words.len() {
+                    words[i] = words[i].to_lowercase();
                 }
             } else if self.case == LetterCase::Upper{
-                for _ in 0..words.len() {
-                    let s: String = words.remove(0);
-                    words.push(s.to_uppercase());
+                for i in 0..words.len() {
+                    words[i] = words[i].to_uppercase();
                 }
             }
-
             if words.len() < 2 {
                 false
             } else {
@@ -241,37 +234,23 @@ pub mod markov {
         }
 
         #[test]
-        fn test_pass_string() {
-            let mut m = markov::new();
-            m.pass_string("who what when where why who what".to_string());
-            assert!(m.generate(10).unwrap() == "who what when where why who what when where why".to_string());
-        }
-
-        #[test]
         fn test_pass() {
             let mut m = markov::new();
-            m.should_strip = true;
             let data = vec!["Hello, how are you?", "What are you going to wear tonight?", "What time is it?"];
+            m.add_filter(markov::non_alphanumeric_regex());
             m.pass(data);
             assert!(m.seeds.contains(&("Hello".to_string(), "how".to_string())));
             m.generate(10).unwrap();
         }
 
-        // Test pass with the 'strip all non alpha-numeric chars' flag
+        // Test pass with the 'strip all non alpha-numeric chars' regex
         #[test]
         fn test_pass_strip() {
             let mut m = markov::new();
-            m.should_strip = true;
             let sentences = vec!["Hello, how are you?"];
+            m.add_filter(markov::non_alphanumeric_regex());
             m.pass(sentences);
             assert!(m.generate(10).unwrap() == "Hello how are you");
-        }
-
-        #[test]
-        fn test_strip() {
-            let m = "Hello".to_string();
-            let n = markov::strip("Hello!");
-            assert!(n == m);
         }
 
         #[test]
@@ -282,8 +261,8 @@ pub mod markov {
             use std::path::Path;
 
             let mut m = markov::new();
-            m.should_strip = false;
             m.case = markov::LetterCase::Lower;
+            m.add_filter(markov::non_alphanumeric_regex());
 
             let mut file = match File::open(&Path::new("testdata/data.txt")) {
                 Err(why) => panic!("couldn't create testdata/data.txt: {}",
@@ -304,6 +283,44 @@ pub mod markov {
             for _ in 1..100 {
                 println!("Generated \"{}\"", m.generate(100).unwrap());
             }
+        }
+
+        #[test]
+        fn test_trump() {
+            use std::error::Error;
+            use std::fs::File;
+            use std::io::prelude::*;
+            use std::path::Path;
+
+            let mut m = markov::new();
+            m.case = markov::LetterCase::Lower;
+            m.add_filter(markov::url_regex());
+            m.add_filter(markov::non_alphanumeric_regex());
+
+            let mut file = match File::open(&Path::new("testdata/trump.txt")) {
+                Err(why) => panic!("couldn't create testdata/trump.txt: {}",
+                            why.description()),
+                Ok(file) => file,
+            };
+
+            let mut s = String::new();
+
+            match file.read_to_string(&mut s) {
+                Err(why) => panic!("couldn't read testdata/trump.txt: {}",
+                                    why.description()),
+                Ok(_) => (),
+            };
+
+            let sentences = s.split("\r").collect::<Vec<&str>>();
+            m.pass(sentences);
+            for _ in 1..100 {
+                println!("Generated \"{}\"", m.generate(100).unwrap());
+            }
+        }
+
+        #[test]
+        fn test_custom_regex() {
+
         }
     }
 }
